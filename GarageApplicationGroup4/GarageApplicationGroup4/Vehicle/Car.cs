aeeb6a6c1783ff876ccd0297706fba7b56e0247b
LAPTOP-48TDI2LP\miral <mirali.mtjjl@gmail.com>
2021-11-03 15:26:48 +0100

    Car delar

--- GarageApplicationGroup4/GarageApplicationGroup4/Vehicle/Car.cs
+++ GarageApplicationGroup4/GarageApplicationGroup4/Vehicle/Car.cs
@@ -6,12 +6,26 @@ using System.Threading.Tasks;
 
 namespace GarageApplicationGroup4
 {
-	public class Car:Vehicle
-	{
-		public Car()
-		{
+    public class Car : Vehicle
+    {
+        public bool isCab { get; set; }
+        public bool hasTowbar { get; set; }
+        public int YearMade { get; set; }
+        public Car()
+        {
 
-		}
-	}
-
+        }
+        public Car(string RegistrationNumber, string Manufacturer, string Model, int YearMade, string Propellant, int NumberOfWheels, string Color, bool isCab, bool hasTowbar)
+        {
+            this.RegistrationNumber = RegistrationNumber;
+            this.Manufacturer = Manufacturer;
+            this.Model = Model;
+            this.YearMade = YearMade;
+            this.Propellant = Propellant;
+            this.NumberOfWheels = NumberOfWheels;
+            this.Color = Color;
+            this.isCab = isCab;
+            this.hasTowbar = hasTowbar;
+        }
+    }
 }
