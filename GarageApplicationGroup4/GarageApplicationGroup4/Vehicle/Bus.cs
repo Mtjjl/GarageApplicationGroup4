c1a9f0ebaa4b69ec125badf939f06eb7b3a423cb
LAPTOP-48TDI2LP\miral <mirali.mtjjl@gmail.com>
2021-11-03 15:28:53 +0100

    buss propeties

--- GarageApplicationGroup4/GarageApplicationGroup4/Vehicle/Bus.cs
+++ GarageApplicationGroup4/GarageApplicationGroup4/Vehicle/Bus.cs
@@ -6,13 +6,26 @@ using System.Threading.Tasks;
 
 namespace GarageApplicationGroup4
 {
+    public class Bus : Vehicle
+    {
+        public bool isDoubleDecker { get; set; }
+        public int numberOfPassenger { get; set; }
+        public int YearMade { get; set; }
+        public Bus()
+        {
 
-	public class Bus:Vehicle
-	{
-		public Bus()
-		{
-
-		}
-	}
-
+        }
+        public Bus(string RegistrationNumber, string Manufacturer, string Model, int YearMade, string Propellant, int NumberOfWheels, string Color, bool isDoubleDecker, int numberOfPassenger)
+        {
+            this.RegistrationNumber = RegistrationNumber;
+            this.Manufacturer = Manufacturer;
+            this.Model = Model;
+            this.YearMade = YearMade;
+            this.Propellant = Propellant;
+            this.NumberOfWheels = NumberOfWheels;
+            this.Color = Color;
+            this.isDoubleDecker = isDoubleDecker;
+            this.numberOfPassenger = numberOfPassenger;
+        }
+    }
 }
