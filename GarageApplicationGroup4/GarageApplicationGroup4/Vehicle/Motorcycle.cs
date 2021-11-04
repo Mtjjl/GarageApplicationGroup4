9bab4fb3717df561f46217e6bbbc2b019e0d8019
Cassandra Heimerwall <cassie.heimerwall@live.se>
2021-11-04 12:03:50 +0100

    Satt properties och parametrar i metod innuti motorcycle

--- GarageApplicationGroup4/GarageApplicationGroup4/Vehicle/Motorcycle.cs
+++ GarageApplicationGroup4/GarageApplicationGroup4/Vehicle/Motorcycle.cs
@@ -8,10 +8,16 @@ namespace GarageApplicationGroup4
 {
 	public class Motorcycle:Vehicle
 	{
-		public Motorcycle()
+        public string WeightClass { get; set; }
+        public int NumberOfCC { get; set; }
+        public Motorcycle()
 		{
-			// motorcykelklassen
 		}
+
+        public Motorcycle(string RegistrationNumber, string Model, string Manufacturer, int YearMade, string Propellent, string Color, int NumberOfWheels, string WeightClass, int NumberOfCC)
+        {
+                
+        }
 	}
 
 }
