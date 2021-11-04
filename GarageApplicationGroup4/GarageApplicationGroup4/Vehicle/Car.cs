f260c9e3d77101bef76cb98a085f15492f646ba9
Sandra Wellbrant <sandrawellbrant@gmail.com>
2021-11-04 12:54:22 +0100

    Har lagt in properties och tillkallat från den ärvda klassen

--- GarageApplicationGroup4/GarageApplicationGroup4/Vehicle/Car.cs
+++ GarageApplicationGroup4/GarageApplicationGroup4/Vehicle/Car.cs
@@ -7,10 +7,25 @@ using System.Threading.Tasks;
 namespace GarageApplicationGroup4
 {
 	public class Car : Vehicle
+		
 	{
+		public bool isCab { get; set; }'
+		public bool hasTowbar { get; set; }
 		public Car()
 		{
 
+		}
+		public Car (bool isCab, bool hasTowbar, string RegistrationNumber, string Model, string Color, string Propellant, string Manufacturer, int NumberOfWheels, int YearMade)
+		{
+			this.isCab = isCab;
+			this.hasTowbar = hasTowbar;
+			this.RegistrationNumber = RegistrationNumber;
+			this.Manufacturer = Manufacturer;
+			this.Propellant = Propellant;
+			this.Model = Model;
+			this.Color = Color;
+			this.NumberOfWheels = NumberOfWheels;
+			this.YearMade = YearMade;
 		}
 	}
 
