213b65b71f54feba25e5d0baf88b1eb78557fbaa
leanderson.fredrik@gmail.com <leanderson.fredrik@gmail.com>
2021-11-05 11:00:16 +0100

    Lagt till GetNewVehicle()

--- GarageApplicationGroup4/GarageApplicationGroup4/Vehicle/Vehicle.cs
+++ GarageApplicationGroup4/GarageApplicationGroup4/Vehicle/Vehicle.cs
@@ -31,6 +31,36 @@ namespace GarageApplicationGroup4
         { get; set; }
 
 
+        public static Vehicle GetNewVehicle()
+        {
+            int choice = Validate.GetValidNumber("What type of vehicle would you like to park?\n[1] Car \n[2] Bus\n[3] Moped\n[4] Motocycle\n[5] Truck", 1, 5);
+            Validate.GetValidPlateNumber(out string plateNumber);
+            string manufacturer = Validate.GetString("What's the manufacturer?");
+            string model = Validate.GetString("What model is it?");
+            string color = Validate.GetString("What color is it?");
+            string propellant = Validate.GetString("What propellant is it?");
+            int wheels = Validate.GetValidNumber("How many wheels does it have?", 2, 8);
+            int yearMade = Validate.GetValidNumber("When was it made?", 1950, 2021);
+
+            switch (choice)
+            {
+                case 1:
+                    return Car.NewCar(plateNumber, manufacturer, model, color, propellant, wheels, yearMade);
+
+                case 2:
+                    return Bus.NewBus(plateNumber, manufacturer, model, color, propellant, wheels, yearMade);
+
+                case 3:
+                    return Moped.NewMoped(plateNumber, manufacturer, model, color, propellant, wheels, yearMade);
+
+                case 4:
+                    return Motorcycle.NewMotorcycle(plateNumber, manufacturer, model, color, propellant, wheels, yearMade);
+
+                case 5:
+                    return Truck.NewTruck(plateNumber, manufacturer, model, color, propellant, wheels, yearMade);
+            }
+            return null;
+        }
 
     }
 }
