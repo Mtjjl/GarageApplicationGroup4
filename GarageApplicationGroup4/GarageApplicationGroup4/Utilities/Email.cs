198426b1e2a02f0e63c086601bda9579e93fb8ac
RobinHagglund <robin.haegglund@gmail.com>
2021-11-09 13:40:28 +0100

    Bytta mail

--- GarageApplicationGroup4/GarageApplicationGroup4/Utilities/Email.cs
+++ GarageApplicationGroup4/GarageApplicationGroup4/Utilities/Email.cs
@@ -8,54 +8,55 @@ namespace GarageApplicationGroup4
     {
         public void Emailer()
         {
-            //Create mimemsg abiject
+            //Skapar ett Mimemessage objekt
             MimeMessage message = new MimeMessage();
             //add the sender info that will appear in the email message
             message.From.Add(new MailboxAddress("Grupp 4", "garagegrupp4@gmail.com"));
 
-            //add the reciver email adress TEST
-            message.To.Add(MailboxAddress.Parse("garagegrupp4@gmail.com"));
+            //Mottagarmejl - Test
+            //message.To.Add(MailboxAddress.Parse("garagegrupp4@gmail.com"));
 
-            //add the reciver email adress MICKES
-            //message.To.Add(MailboxAddress.Parse("msdata1010@gmail.com"));
+            //Mottagarmail - MICKES
+            message.To.Add(MailboxAddress.Parse("msdata1010@gmail.com"));
 
-            //add message subject
+            //Meddelande ämne
             message.Subject = "Din digitala faktura från din parkering hos GarageGrupp4.";
 
-            //add the message as plain text
+            //Meddelande
             message.Body = new TextPart("plain")
             {
                 Text = @"Tack för din senaste vistelse! Här kommer din faktura. Vänligen betala in 5000:- inom 14 dagar, dröjsmålsränta om 12321380812% tillkommer."
             };
 
-            //Email sender credidentials
+            //Avsändarinlogg 
             string emailAdress = "garagegrupp4@gmail.com";
 
             string password = "abcd.123";
 
-            //create smtp clint
+            //Skapar en smtp cleint
             SmtpClient client = new SmtpClient();
 
             try
             {
-                //Connect to the gmail smtp server, port 465 ssl enabled.
+                //Uppkoppling till gmail smtp server, port 465 ssl enabled.
                 client.Connect("smtp.gmail.com", 465, true);
                 client.Authenticate(emailAdress, password);
                 client.Send(message);
 
-                //display a message if no exception thrown.
+                //Visa detta meddelande om det inte finns en exception.
                 Console.WriteLine("Email sent!");
             }
             catch (Exception ex)
             {
-                //in case of error display the msg
+                //Om det blir ett fel, visa  vad.
                 Console.WriteLine(ex.Message);
             }
             finally
             {
-                //at any case always disconnect from the client
+                //Alltid - avbryt kopplingen till klienten.
                 client.Disconnect(true);
-                //and dispose of the client object
+
+                //Radera klient objektet.
                 client.Dispose();
             }
         }
